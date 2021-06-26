e49ddfdb4d7b5736357d80b09623f8ab521c0fb4
BenhurKulzer <bdskulzer@brazpine.com>
2021-06-25 22:17:11 -0300

    feat: add auth with github, stripe payments and faunaDB

---
+++ src/pages/index.tsx
@@ -0,0 +1,59 @@
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
+
+import styles from './home.module.scss';
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | IG.News</title>
+      </Head>
+    
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>🖐 Hey, Welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+
+          <p>
+            Get access to all the publications <br />
+            <span>for {product.amount} month</span>
+
+            <SubscribeButton priceId={product.priceId} />
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="girl coding" />
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1J5ERTDHwjI6oyWHZtzKCIrq');
+
+  const product = {
+    pridceId: price.id,
+    amount: new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 horas
+  }
+}
